f05464e9ecc0cbe2ed8f2fb1c34ea0cf16a0b433
Artan Ebibi <a.ebibi123@gmail.com>
2025-06-30 18:36:39 +0200

    kubernetes deployment

--- food-delivery-backend/src/main/java/mk/ukim/finki/fooddeliverybackend/config/security/JwtSecurityWebConfig.java
+++ food-delivery-backend/src/main/java/mk/ukim/finki/fooddeliverybackend/config/security/JwtSecurityWebConfig.java
@@ -37,7 +37,7 @@ public class JwtSecurityWebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://3.71.199.12:3000", "http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("http://3.71.199.12:3000", "http://localhost:3000", "http://food-delivery.com"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
